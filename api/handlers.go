0397264706c4dff6be4d372783cf4b0099b9a12f
polypmer <exorable.ludos@gmail.com>
2016-10-11 10:56:14 -0400

    NewComment Handler

--- api/handlers.go
+++ api/handlers.go
@@ -3,8 +3,11 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/polypmer/mappyr/database"
@@ -60,4 +63,47 @@ func ShowComments(w http.ResponseWriter, r *http.Request) {
 }
 
 /* DB Write */
-//
+// NewComment Writes a new comment to db
+// If no user is specified, use 0 for id
+func NewComment(w http.ResponseWriter, r *http.Request) {
+	// This is taking a POST method
+	var comment database.Comment
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = r.Body.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	// Unmarshal, stick into my struct
+	err = json.Unmarshal(body, &comment)
+	// Unable to marshal?:
+	if err != nil {
+		// if shit don't work out
+		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+		w.WriteHeader(http.StatusUnprocessableEntity) //422
+		err = json.NewEncoder(w).Encode(err)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	// Now I have a comment json object
+	comment.Date = time.Now()
+	// Check for Authentication, and in that case,
+	// Add the user to the comment
+
+	id, err := database.WriteComment(db, comment)
+	if err != nil {
+		fmt.Println(err)
+	}
+	comment.Id = id
+	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+	w.WriteHeader(http.StatusCreated)
+
+	// Feed it back into the response Writer
+	err = json.NewEncoder(w).Encode(comment)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
---
+++ curl.sh
@@ -0,0 +1,1 @@
+curl -H "Content-Type: application/json" -d '{"text":"This place sux0rs", "lat":"41.221", "lon":"-71.666"}' http://localhost:8080/new
