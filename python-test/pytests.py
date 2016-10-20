36533d805b4c2b744c3a5d2a57d523d95177744d
polypmer <exorable.ludos@gmail.com>
2016-10-20 09:36:51 -0400

    Added some more python functions, but theyre not really tests yet

--- python-test/pytests.py
+++ python-test/pytests.py
@@ -1,10 +1,15 @@
 #from nose.tools import assert_true
 import requests
 
+# Example tokens with id
+
 token_1 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyLWlkIjoiMSIsImV4cCI6MTQ3Njk3MTQ2NywiaXNzIjoibG9jYWxob3N0OjgwODAifQ.wv1UgclK5uKUYYZFpnx4DjLcHlinirTfzL0nhmrJ7gc"
 
 token_8 = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyLWlkIjoiOCIsImV4cCI6MTQ3Njk3MzEwNCwiaXNzIjoibG9jYWxob3N0OjgwODAifQ.NnxhEH7ETklp8H_hkNNxFaJpHt0s4TdE2gWVRpSE39Q"
 
+##########################################################################################
+#USER ACTIONS
+##########################################################################################
 def signup():
     """Create a new user"""
     r = requests.post('http://localhost:8080/signup', json ={"username":"Wrinkle",
@@ -18,7 +23,40 @@ def signup():
 
 def comment():
     """Post a comment with a token in the headers"""
-    r = requests.post('http://localhost:8080/new', json={"title":"SUsh1","description":"THIS Rox0rs", "latitude":45, "longitude":88}, headers={"Authentication":token_8})
+    r = requests.post('http://localhost:8080/new',
+                      json={"title":"SUsh1","description":"THIS Rox0rs",
+                            "latitude":45, "longitude":88},
+                      headers={"Authentication":token_8})
+
+    print(r.status_code)
+    print(r.json())
+
+
+def upvote(comment):
+    """UpVOTE!"""
+    r = requests.get('http://localhost:8080/upvote/'+ comment, headers={"Authentication":token_8})
+    print(r.status_code)
+    print(r.json())
+
+def downvote(comment):
+    """downVOTE!"""
+    r = requests.get('http://localhost:8080/downvote/'+ comment, headers={"Authentication":token_8})
+    print(r.status_code)
+    print(r.json())
+
+##########################################################################################
+#Get Votes or COmments of User
+##########################################################################################
+def user_votes(id):
+    """User votes takes the id, but pass a string"""
+    r = requests.get("http://localhost:8080/votes/"+id)
+
+    print(r.status_code)
+    print(r.json())
+
+def user_comments(id):
+    """User comments takes the id, but pass a string"""
+    r = requests.get("http://localhost:8080/comments/"+id)
 
     print(r.status_code)
     print(r.json())
